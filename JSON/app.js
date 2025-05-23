6914c0ac04317e407f49fa4ddb1f6284763b39d1
Rodrigo Branco <stikadinho@proton.me>
2025-05-23 17:56:12 -0300

    Update app.js

--- JSON/app.js
+++ JSON/app.js
@@ -4,7 +4,31 @@ function lerDados() {
     const dadosBrutos = fs.readFileSync('dados.json');
     const dados = JSON.parse(dadosBrutos);
 
-    return console.log(dados);
+    // return console.log(dados);
+    return dados
 }
 
 lerDados();
+
+function listarProdutos() {
+    const produtos = lerDados();
+    produtos.forEach(produto => {
+
+        console.log(`
+            Nome: ${produto.nome},
+            Preço: ${produto.preco}`);
+    });
+}
+
+listarProdutos();
+
+function calcularValorEstoque() {
+    const produtos = lerDados();
+    let valorTotal = 0;
+    produtos.forEach(produto => {
+        valorTotal += produto.preco * produto.quantidade
+    });
+    console.log(`\nValor Total do Estoque: ${valorTotal.toFixed(2)}`)
+}
+
+calcularValorEstoque();
